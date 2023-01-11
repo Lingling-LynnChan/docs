a7d62795ca468384d91d10fdd2e2b2676059ae78
Lingling <97364813+Lingling-LynnChan@users.noreply.github.com>
2023-01-11 23:58:03 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="zh">
+  <head>
+    <meta charset="utf-8">
+    <title>Lingling's blog</title>
+  </head>
+  <body align="center">
+    <?php echo '暂无内容';?>
+  </body>
+</html>
